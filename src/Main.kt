fbe8664ddd5c03ed39b6d83ead933dedda101547
Mafi2482 <maksudurrahmanmafi0@gmail.com>
2026-01-03 16:24:20 +0600

    Basic kotlin

---
+++ src/Main.kt
@@ -0,0 +1,20 @@
+fun ans() {
+    print("Hello World\n")
+    for (i in 1 until  10 step 2) print("Hello Bangladesh\n");
+
+}
+
+fun main() {
+//    for (i in 1..10) println(i)
+//    ans()
+//    var a: Int =10//var data changeable
+//    a=15
+////    print(a)
+//    val b: Double =10.3
+////    b=17
+//    print(b)
+
+   var a =10//decearation and initialization
+    var b : Int
+   a=10
+}
