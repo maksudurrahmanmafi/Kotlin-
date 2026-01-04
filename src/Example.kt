ce9313f5aafd311863f999e717ef8bb71d8c61af
Mafi2482 <maksudurrahmanmafi0@gmail.com>
2026-01-04 17:44:41 +0600

    User Input

---
+++ src/Example.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+fun main(){
+    val sc = Scanner(System.`in`)
+    print("Enter 1st Number: ")
+
+    val x = readln().toInt()
+    print("Enter Name: ")
+
+    val y = sc.next()
+
+    print(x)
+    print(y)
+
+}
---
+++ src/hello.kt
@@ -0,0 +1,13 @@
+fun main(){
+//    val x : Int=-160
+//    val y: Byte=x.toByte()
+//    print(y)
+    var x =100
+    x++
+    println(x)
+    val x1: Byte= 50
+    println(x1)
+    for(i in 1..x1){
+        print("$i \n")
+    }
+}
